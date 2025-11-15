7141d388890568bb39550911bc46a3ab8395a707
Nick Bourgeois <77581387+Nick676191@users.noreply.github.com>
2025-11-15 09:00:57 -0600

    Implement a square grid of div containers within their parent container in the DOM

--- sketch-script.js
+++ sketch-script.js
@@ -3,6 +3,6 @@ const container = document.querySelector("div");
 // creating 256 boxes for the 16x16 grid of divs
 for (let i = 0; i < 256; i++) {
     const divGrid = document.createElement("div");
-    divGrid.className = "box-" + i;
+    divGrid.className = "box";
     container.appendChild(divGrid);
 };
